93680e5352c0a4455309083c97121022d4966d66
Anna_Maslova <personnel2003@yandex.ru>
2022-11-24 13:38:56 +0300

    lgical operators

---
+++ logical.py
@@ -0,0 +1,21 @@
+#логические операции
+# >, >=, <, <=, ==, != (неравенство)
+# not, and, or 
+# is, is not, in, not in
+# gen 
+
+#a = 1<4 and 5>2
+#print(a) 
+b = 'qwe' # b = [1,2]
+c = 'qwe' # c = [1,2] получим тоже true
+print (b == c)
+
+#f = 1>2 or 4<6
+#print (f)
+
+f = [1,2,3,4,]
+#print(f)
+#print(not 2 in f)
+
+is_odd = not f[0] % 2  #0 - это ложь, а 1 - истина поэтому 
+print (is_odd)
