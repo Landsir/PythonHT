e5b4edda0c1579281b2cdfaa0bc68ef25b4af9a3
Anna_Maslova <personnel2003@yandex.ru>
2022-11-25 23:48:02 +0300

    Lesson1_4

---
+++ workshops/Lesson1_4.py
@@ -0,0 +1,11 @@
+# n = float(input('Input a number: '))
+# num = n*10
+# if (num%10==0):
+#     print('there is no ')
+# else:
+#     print(int(n*10%10))
+
+f = input('Input a number: ')
+print(f[f.find(".")+1])
+
+
