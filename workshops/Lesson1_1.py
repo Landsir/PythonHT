349c0b4cd10eb01511b94ff96ca8e49f2386c367
Anna_Maslova <personnel2003@yandex.ru>
2022-11-25 20:45:13 +0300

    Lesson1_2

--- workshops/Lesson1_1.py
+++ workshops/Lesson1_1.py
@@ -3,6 +3,8 @@ a = int(input())
 print('Input b')
 b= int(input())
 if b == a*a:
-    print('the number {b} is the square of the number {a} ')
+    print(f'the number {b} is the square of the number {a} ')
+elif a == b*b:
+    print (f'the number {a} is the square of the number {b}')
 else:
-    print ('the number {b} is not the square of the number {a}')
+    print (f'numbers {a} and {b} are not squares each other')
