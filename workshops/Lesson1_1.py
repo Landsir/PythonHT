0bfb70574c494b5227c0375e197d9cd8a5c86e72
Anna_Maslova <personnel2003@yandex.ru>
2022-11-25 19:08:55 +0300

    initial commit

---
+++ workshops/Lesson1_1.py
@@ -0,0 +1,8 @@
+print('Input a')
+a = int(input())
+print('Input b')
+b= int(input())
+if b == a*a:
+    print('the number {b} is the square of the number {a} ')
+else:
+    print ('the number {b} is not the square of the number {a}')
