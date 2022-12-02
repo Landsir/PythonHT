b124a95618c700a16439f403a55524fdfc628aea
Anna_Maslova <personnel2003@yandex.ru>
2022-12-02 22:27:39 +0300

    hometask 2

---
+++ hometasks/Hometask1_2.py
@@ -0,0 +1,32 @@
+#Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+# x = int(input('input x: '))
+# y = int(input('input y: '))
+# z = int(input('input z: '))
+
+""" Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.  """
+
+
+def inputNumbers(x):
+    xyz = ["x", "y", "z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
