f753dccfb24fefb1bca1ebd1b3286b29e57d8bac
Anna_Maslova <personnel2003@yandex.ru>
2022-12-08 09:47:44 +0300

    lesson 2_1

--- workshops/Lesson2_1.py
+++ workshops/Lesson2_1.py
@@ -2,11 +2,10 @@
 # в том числе пробел, стоит 60 копеек. Ответ дайте в рублях и копейках. 
 
 line = str(input('input a message '))
-print(len(line))
 cost = len(line)
 coins = cost*60
 if coins > 100:
-    rubls = round(coins/100)
+    rubls = coins//100
     coins = coins % 100
     print(f'your message costs {rubls} rubls and {coins} coins')
 else:
