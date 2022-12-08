a0b45b72e3808769acff2da3f1800cdd3a27e840
Anna_Maslova <personnel2003@yandex.ru>
2022-12-08 16:29:58 +0300

    lesson 2_4

---
+++ workshops/Lesson 2_4.py
@@ -0,0 +1,9 @@
+#Напишите программу, в которой пользователь будет задавать две строки, а программа - определять количество 
+#вхождений одна в другую. 
+
+line1 = input('input first message: ')
+line2 = input('input second message: ')
+
+print(line1.count(line2))
+
+
